fea608f313d327334c02fa6c299c3750bbe855ff
Aditya <lapratomo24@yahoo.com>
2024-08-07 19:04:11 +0200

    add function to check user guess

--- main.py
+++ main.py
@@ -11,10 +11,21 @@ def format_data(clubs):
     club_origin = clubs['origin']
     return f"{club_name}, a {club_league} club, based in {club_origin}."
 
+def check_guess(guess, club_a, club_b):
+    follower_count_a = club_a['follower_ig_millions']
+    follower_count_b = club_b['follower_ig_millions']
+    if follower_count_a > follower_count_b:
+        return guess == 'a'
+    elif follower_count_b > follower_count_a:
+        return guess == 'b'
+
 club_a = random.choice(clubs)
 club_b = random.choice(clubs)
 while club_a == club_b:
     club_b = random.choice(clubs)
+    
+score = 0
+run_game = True
 
 print(logo)
 
@@ -23,3 +34,7 @@ print(f"Compare A: {format_data(club_a)}")
 print(vs)
 
 print(f"Against B: {format_data(club_b)}")
+
+user_guess = input("Which club has more followers on IG? Type 'A' or 'B': ").lower()
+
+check_guess(user_guess, club_a, club_b)
